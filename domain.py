ae8e849d4fc35cdf95575eb0bab9eb474ee03788
MoeList <88638350+MoeList@users.noreply.github.com>
2021-08-11 06:52:04 +0800

    Update domain.py

--- domain.py
+++ domain.py
@@ -4,24 +4,25 @@ import requests
 import datetime
 import time
 
-qq='接收信息的QQ'
+qq='接收信息QQ'
 port='12513'
-domain='要监控的域名'
 
 if __name__ == '__main__':
     while (1==1):
-        requests.get('http://127.0.0.1:' + port + '/send_private_msg?user_id=' + qq + '&message=域名检测启动成功')
-        res = requests.get('https://api.moekee.com/whois/' + domain)
-        res = res.text.split("\n")
-        #print(res)
-        for a in res:
-            b = a
-            if 'Registry Expiry Date:' in a:
-                break
-        res = b[22:-1].split('T')
-        d1 = datetime.datetime.strptime(res[0], '%Y-%m-%d')
-        d2 = datetime.datetime.now()
-        res = (d1 - d2)
-        if res.days < 90:
-            requests.get('http://127.0.0.1:'+port+'/send_private_msg?user_id='+qq+'&message=域名到期警告！还有'+str(res.days)+'天域名到期！')
-        time.sleep(86400)
+        with open('domainlist.txt', "r") as f:
+            for line in f.readlines():
+                domain = line.strip('\n')
+                res = requests.get('https://api.moekee.com/whois/'+ str(domain))
+                res = res.text.split('\n')
+                for a in res:
+                    b = a
+                    if 'Registry Expiry Date:' in a:
+                        break
+                res = b[22:-1].split('T')
+                d1 = datetime.datetime.strptime(res[0], '%Y-%m-%d')
+                d2 = datetime.datetime.now()
+                res = (d1 - d2)
+                if res.days < 60:  #想要的时间
+                    requests.get('http://127.0.0.1:' + port + '/send_private_msg?user_id=' + qq + '&message=域名到期提醒！！！\n''域名：'+str(domain)+'\n还有 '+str(res.days)+' 天域名到期！')
+                    time.sleep(5)  #多个域名检测中间间隔秒数（没试过 0 秒）
+        time.sleep(86400)   #多少秒跑一次
