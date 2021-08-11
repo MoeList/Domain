c1384286abe21851e1bd3599408665a1231cbad8
MoeList <88638350+MoeList@users.noreply.github.com>
2021-08-11 08:44:45 +0800

    Update domain.py

--- domain.py
+++ domain.py
@@ -22,7 +22,12 @@ if __name__ == '__main__':
                 d1 = datetime.datetime.strptime(res[0], '%Y-%m-%d')
                 d2 = datetime.datetime.now()
                 res = (d1 - d2)
-                if res.days < 60:  #想要的时间
-                    requests.get('http://127.0.0.1:' + port + '/send_private_msg?user_id=' + qq + '&message=域名到期提醒！！！\n''域名：'+str(domain)+'\n还有 '+str(res.days)+' 天域名到期！')
-                    time.sleep(5)  #多个域名检测中间间隔秒数（没试过 0 秒）
+                if res.days < 0:  #想要的时间
+                    requests.get('http://127.0.0.1:' + port + '/send_private_msg?user_id=' + qq + '&message=域名删除提醒！！！\n''域名：' + str(domain) + '\n已到期 ' + str(res.days) + ' 天！')
+                    time.sleep(5)  # 多个域名检测中间间隔秒数（没试过 0 秒）
+                elif res.days <= 60:
+                    requests.get('http://127.0.0.1:' + port + '/send_private_msg?user_id=' + qq + '&message=域名到期提醒！！！\n''域名：' + str(domain) + '\n还有 ' + str(res.days) + ' 天域名到期！')
+                    time.sleep(5)  # 多个域名检测中间间隔秒数（没试过 0 秒）
+                elif res.days > 60:
+                    time.sleep(5)  # 多个域名检测中间间隔秒数（没试过 0 秒）
         time.sleep(86400)   #多少秒跑一次
